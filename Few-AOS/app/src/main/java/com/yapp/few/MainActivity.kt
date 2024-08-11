f8f663ab264b56d3fdf2075505f779e995c78688
송영모 <mooyoung2309@gmail.com>
2024-08-11 15:18:19 +0900

    feat: 안드로이드 웹뷰 추가

--- Few-AOS/app/src/main/java/com/yapp/few/MainActivity.kt
+++ Few-AOS/app/src/main/java/com/yapp/few/MainActivity.kt
@@ -1,47 +1,56 @@
 package com.yapp.few
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.yapp.few.ui.theme.FewTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             FewTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    WebViewContainer(url = "https://www.fewletter.com")
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun WebViewContainer(url: String) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { context ->
+                WebView(context).apply {
+                    settings.javaScriptEnabled = true
+                    webViewClient = CustomWebViewClient()
+                    loadUrl(url)
+                }
+            }
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FewTheme {
-        Greeting("Android")
+    private class CustomWebViewClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            if (url != null && !url.contains("fewletter.com")) {
+                view?.context?.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                return true
+            }
+            return false
+        }
     }
 }
